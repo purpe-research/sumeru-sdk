6a96d386515d1820062a544cdffb13482a8953da
Rohit Jalan <r0h17@purpe.com>
2021-09-04 12:19:39 +0530

    Add toolchain binaries

---
+++ riscv-gnu-toolchain/lib/gcc/riscv32-unknown-elf/9.2.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/home/r0h17/workspace-vhdl/sumeru/compile/riscv-gnu-toolchain/riscv-gcc/configure --target=riscv32-unknown-elf --prefix=/home/r0h17/workspace-vhdl/sumeru/riscv-gnu-toolchain --disable-shared --disable-threads --enable-languages=c,c++ --with-system-zlib --enable-tls --with-newlib --with-sysroot=/home/r0h17/workspace-vhdl/sumeru/riscv-gnu-toolchain/riscv32-unknown-elf --with-native-system-header-dir=/include --disable-libmudflap --disable-libssp --disable-libquadmath --disable-libgomp --disable-nls --disable-tm-clone-registry --src=.././riscv-gcc --disable-multilib --with-abi=ilp32 --with-arch=rv32ima --with-tune=rocket 'CFLAGS_FOR_TARGET=-Os   -mcmodel=medlow' 'CXXFLAGS_FOR_TARGET=-Os   -mcmodel=medlow'";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "abi", "ilp32" }, { "arch", "rv32ima" }, { "tune", "rocket" } };
